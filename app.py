ed1f895f88d9588fe4d2a246065348f2d5688195
sudongcu <sudongcu@gmail.com>
2021-02-11 01:38:48 +0900

    col, row 순서변경 / 테이블 그리기 / css 추가 및 versining

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from playgame import game
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,16 +10,19 @@ def main():
 
 @app.route('/start')
 def start():
-    col = int(request.args['col'])
     row = int(request.args['row'])
+    col = int(request.args['col'])
 
-    g = game(col, row)
+    g = game(row, col)  
     g.setGame()
 
     data = {'map':g.game_map, 'hider':g.hider}
     return jsonify(data)
 
-
+# @app.route('/seek')
+# def seek():
+    
+#     return  jsonify('')
 
 if __name__ == '__main__':
     app.run(debug=True)
